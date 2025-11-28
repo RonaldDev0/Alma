b02f8d6ff15e218da316c0f44a2a9924d6f00901
Ronald_dev <ronaldzamoradev@gmail.com>
2025-11-28 12:16:06 -0500

    feat: debounce in search barr

--- src/app/(public)/list/search.tsx
+++ src/app/(public)/list/search.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import { Input } from '@/components/ui/input'
 import { TableData, type TRecord } from './table'
 
@@ -11,20 +11,29 @@ type IProps = {
 
 export function Data({ records }: IProps) {
   const [input, setInput] = useState('')
+  const [debouncedInput, setDebouncedInput] = useState('')
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedInput(input)
+    }, 300)
+
+    return () => clearTimeout(handler)
+  }, [input])
 
   const handleChange = ({ target: { value } }: any) => {
     setInput(value)
   }
 
   const filteredData = useMemo(() => {
-    if (!input) return records
-    const lowerInput = input.toLowerCase()
+    if (!debouncedInput) return records
+    const lowerInput = debouncedInput.toLowerCase()
     return records.filter(
       record =>
         record.reference.toLowerCase().includes(lowerInput) ||
         record.brand.toLowerCase().includes(lowerInput)
     )
-  }, [input, records])
+  }, [debouncedInput, records])
 
   return (
     <>
