2a252da40d7cfaaf3c04a04958eb152b356030bf
Ronald_dev <ronaldzamoradev@gmail.com>
2026-09-16 12:44:03 -0500

    feat: change header name to EnterCT

--- src/components/sidebar/header.tsx
+++ src/components/sidebar/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
                 <Command className='size-4' />
               </div>
               <div className='grid flex-1 text-left text-sm leading-tight'>
-                <span className='truncate font-medium'>Alma</span>
+                <span className='truncate font-medium'>EnterCT</span>
                 <span className='truncate text-xs text-muted-foreground'>Enterprise</span>
               </div>
             </Link>
